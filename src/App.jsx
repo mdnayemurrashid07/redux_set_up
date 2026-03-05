22d5cdbf3ee9e7f394e67f8ee4d283cd03870036
gitnayemurrashid <nrsrabon17@gmail.com>
2026-03-05 15:02:23 +0600

    nothing changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className=' mx-auto max-w-7xl bg-sky-200 '>
+    <div className=' mx-auto max-w-xl bg-sky-200 '>
       app
     </div>
   )
